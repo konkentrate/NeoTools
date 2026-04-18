c759c27cc1cc22f3c2ac07beec041849faadde23
Adrian Top <top.adrian@outlook.com>
2026-04-18 08:49:01 +0200

    Changed copper mining speed to sensible value

--- src/main/java/com/konkentrate/neotools/item/ModTiers.java
+++ src/main/java/com/konkentrate/neotools/item/ModTiers.java
@@ -11,7 +11,7 @@ public class ModTiers {
     public static final Tier COPPER = new SimpleTier(
             ModTags.Blocks.INCORRECT_FOR_COPPER_TOOL,
             200,   // durability
-            15.0f,  // mining speed
+            5.0f,  // mining speed
             1.5f,  // attack damage bonus
             20,    // enchantability
             () -> Ingredient.of(ItemTags.create(
